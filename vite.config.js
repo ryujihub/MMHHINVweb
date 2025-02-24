cf0564b08ee72af486c19e252eb98f6ce66b87ee
ryujihub <caburnayandrey09@gmail.com>
2025-02-24 15:35:20 +0800

    3rd nq

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/hardware-inventory/',
   server: {
     port: 3000,
     host: true
