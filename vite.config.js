1e970cda3483f9cbdb66bc2896330f2cc39a99ac
ryujihub <caburnayandrey09@gmail.com>
2025-07-03 23:38:20 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/hardware-inventory/',
+  base: '/',
   server: {
     port: 3000,
     host: true
